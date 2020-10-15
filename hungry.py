0c575036a370502c1303b0e9cc307bdb0368ed27
exception29 <72030614+exception29@users.noreply.github.com>
2020-10-15 22:18:26 +0530

    Update hungry.py

--- hungry.py
+++ hungry.py
@@ -2,7 +2,6 @@ hungry = input("Are you hungry")
 if hungry =="yes":
     print("Eat samosas")
     print("Eat pizza")
-    print("Eat burger")
-    print("Eat sandwiches")
+  
 else:
     print("Continue wid your work")
